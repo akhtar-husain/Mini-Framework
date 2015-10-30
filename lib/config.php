07b7356266538d59d5956755b36b9037417eb4da
Akhtar Husain <akhtar4660@gmail.com>
2015-10-30 12:59:27 +0530

    New features added - Akhtar

--- lib/config.php
+++ lib/config.php
@@ -9,14 +9,10 @@
 ob_start();
 session_start();
 
-if (version_compare(phpversion(), '5.4.0', '<') == true) {
-	exit('PHP 5.4+ Required');
-}
-
 const HOSTNAME = 'localhost';
-const DBNAME = 'YourDBName';
-const USERNAME = 'YourUserName';
-const PASSWORD = 'YourPassword';
+const DBNAME = 'akhtar_test';
+const USERNAME = 'root';
+const PASSWORD = '';
 
 const DS = DIRECTORY_SEPARATOR;
 const ENVIRONMENT = 'development'; // OR production => live
@@ -39,6 +35,113 @@ define( 'BASEPATH', $basepath );
 define( 'BASEURL', $baseurl );
 define( 'CURRENT_PAGE', $curPage );
 
+$mimes = array(	'hqx'	=>	'application/mac-binhex40',
+			'cpt'	=>	'application/mac-compactpro',
+			'csv'	=>	array('text/x-comma-separated-values', 'text/comma-separated-values', 'application/octet-stream', 'application/vnd.ms-excel', 'application/x-csv', 'text/x-csv', 'text/csv', 'application/csv', 'application/excel', 'application/vnd.msexcel'),
+			'bin'	=>	'application/macbinary',
+			'dms'	=>	'application/octet-stream',
+			'lha'	=>	'application/octet-stream',
+			'lzh'	=>	'application/octet-stream',
+			'exe'	=>	array('application/octet-stream', 'application/x-msdownload'),
+			'class'	=>	'application/octet-stream',
+			'psd'	=>	'application/x-photoshop',
+			'so'	=>	'application/octet-stream',
+			'sea'	=>	'application/octet-stream',
+			'dll'	=>	'application/octet-stream',
+			'oda'	=>	'application/oda',
+			'pdf'	=>	array('application/pdf', 'application/x-download'),
+			'ai'	=>	'application/postscript',
+			'eps'	=>	'application/postscript',
+			'ps'	=>	'application/postscript',
+			'smi'	=>	'application/smil',
+			'smil'	=>	'application/smil',
+			'mif'	=>	'application/vnd.mif',
+			'xls'	=>	array('application/excel', 'application/vnd.ms-excel', 'application/msexcel'),
+			'ppt'	=>	array('application/powerpoint', 'application/vnd.ms-powerpoint'),
+			'wbxml'	=>	'application/wbxml',
+			'wmlc'	=>	'application/wmlc',
+			'dcr'	=>	'application/x-director',
+			'dir'	=>	'application/x-director',
+			'dxr'	=>	'application/x-director',
+			'dvi'	=>	'application/x-dvi',
+			'gtar'	=>	'application/x-gtar',
+			'gz'	=>	'application/x-gzip',
+			'php'	=>	'application/x-httpd-php',
+			'php4'	=>	'application/x-httpd-php',
+			'php3'	=>	'application/x-httpd-php',
+			'phtml'	=>	'application/x-httpd-php',
+			'phps'	=>	'application/x-httpd-php-source',
+			'js'	=>	'application/x-javascript',
+			'swf'	=>	'application/x-shockwave-flash',
+			'sit'	=>	'application/x-stuffit',
+			'tar'	=>	'application/x-tar',
+			'tgz'	=>	array('application/x-tar', 'application/x-gzip-compressed'),
+			'xhtml'	=>	'application/xhtml+xml',
+			'xht'	=>	'application/xhtml+xml',
+			'zip'	=>  array('application/x-zip', 'application/zip', 'application/x-zip-compressed'),
+			'mid'	=>	'audio/midi',
+			'midi'	=>	'audio/midi',
+			'mpga'	=>	'audio/mpeg',
+			'mp2'	=>	'audio/mpeg',
+			'mp3'	=>	array('audio/mpeg', 'audio/mpg', 'audio/mpeg3', 'audio/mp3'),
+			'aif'	=>	'audio/x-aiff',
+			'aiff'	=>	'audio/x-aiff',
+			'aifc'	=>	'audio/x-aiff',
+			'ram'	=>	'audio/x-pn-realaudio',
+			'rm'	=>	'audio/x-pn-realaudio',
+			'rpm'	=>	'audio/x-pn-realaudio-plugin',
+			'ra'	=>	'audio/x-realaudio',
+			'rv'	=>	'video/vnd.rn-realvideo',
+			'wav'	=>	array('audio/x-wav', 'audio/wave', 'audio/wav'),
+			'bmp'	=>	array('image/bmp', 'image/x-windows-bmp'),
+			'gif'	=>	'image/gif',
+			'jpeg'	=>	array('image/jpeg', 'image/pjpeg'),
+			'jpg'	=>	array('image/jpeg', 'image/pjpeg'),
+			'jpe'	=>	array('image/jpeg', 'image/pjpeg'),
+			'png'	=>	array('image/png',  'image/x-png'),
+			'tiff'	=>	'image/tiff',
+			'tif'	=>	'image/tiff',
+			'css'	=>	'text/css',
+			'html'	=>	'text/html',
+			'htm'	=>	'text/html',
+			'shtml'	=>	'text/html',
+			'txt'	=>	'text/plain',
+			'text'	=>	'text/plain',
+			'log'	=>	array('text/plain', 'text/x-log'),
+			'rtx'	=>	'text/richtext',
+			'rtf'	=>	'text/rtf',
+			'xml'	=>	'text/xml',
+			'xsl'	=>	'text/xml',
+			'mpeg'	=>	'video/mpeg',
+			'mpg'	=>	'video/mpeg',
+			'mpe'	=>	'video/mpeg',
+			'qt'	=>	'video/quicktime',
+			'mov'	=>	'video/quicktime',
+			'avi'	=>	'video/x-msvideo',
+			'movie'	=>	'video/x-sgi-movie',
+			'doc'	=>	'application/msword',
+			'docx'	=>	array('application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/zip'),
+			'xlsx'	=>	array('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/zip'),
+			'word'	=>	array('application/msword', 'application/octet-stream'),
+			'xl'	=>	'application/excel',
+			'eml'	=>	'message/rfc822',
+			'json' => array('application/json', 'text/json')
+		);
+	function _print_r($var)
+	{
+		if( empty($var) )
+			return false;
+			
+		print "<fieldset style=\"border: 1px solid black; font:12px 'Courier new',monospace; padding:10px; margin:10px 0;\">";
+		print "<legend>";
+		print "<b>PRINT RESULT</b>";
+		print "</legend>";
+		print "<pre>";
+		print_r($var);
+		print "</pre>";
+		print "</fieldset>";
+	}
+
 /** ========== C O N S T A N T   E N D S   H E R E ============ **/
 
 /**
@@ -47,23 +150,10 @@ define( 'CURRENT_PAGE', $curPage );
  *
  */
 if( file_exists(BASEPATH . 'vendor') ){
-	require_once BASEPATH ."vendor/autoload.php";
-}
-else{
-	spl_autoload_register( function ($class) {
-	    if( file_exists(BASEPATH . 'lib' . DS . $class . '.class.php') ){    	
-	    	require_once BASEPATH . 'lib' . DS . $class . '.class.php';
-	    }
-	} );
-	/*
-	 * L O A D I N G   T A B L E   F I L E S 
-	 */
-	spl_autoload_register( function ($class) {
-	    if( file_exists(BASEPATH . 'lib' . DS . 'tables' . DS . $class . '.class.php') ){    	
-	    	require_once BASEPATH . 'lib' . DS . 'tables' . DS . $class . '.class.php';
-	    }
-	} );
-	require_once BASEPATH . "lib" . DS . "functions.php";
+	//require BASEPATH ."vendor/autoload.php";
+	require __DIR__.'/../vendor/autoload.php';
 }
 
+use App\Functions;
+$func = new Functions();
 /** =========== F I L E   L O A D I N G   E N D S   H E R E  =========== **/
--- lib/functions.php
+++ lib/functions.php
@@ -1,193 +1,180 @@
 <?php
-namespace App\Functions;
+namespace App;
 /*
  * @author 	: 	Akhtar Husain <akhtar4660@gmail.com>
  * @package : 	Admin Panel
  * @version : 	1.0
  */
-
-function getUriSegment( $segment = 0 )
-{
-	$uri = $_SERVER['REQUEST_URI'];
-	$uri = explode('/', $uri);
-	return $uri[$segment];
-}
-
-function encryptURL($string)
-{
-  $data = base64_encode($string);
-  $data = str_replace(array('+','/','='),array('-','_',':'),$data);
-  return $data;
-}
-
-function decryptURL($string)
-{
-  $data = str_replace(array('-','_',':'),array('+','/','='),$string);
-  $mod4 = strlen($data) % 4;
-  if ($mod4) {
-    $data .= substr('====', $mod4);
-  } 
-  return base64_decode($data);
-}
-
-function isValidURL($url)
-{
-	$regex = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$^"; 
-	if(preg_match($regex, $url))
+class Functions
+{	
+	static function getUriSegment( $segment = 0 )
 	{
-		if(strstr($url,'www'))
+		$uri = $_SERVER['REQUEST_URI'];
+		$uri = explode('/', $uri);
+		return $uri[$segment];
+	}
+
+	static function encryptURL($string)
+	{
+	  $data = base64_encode($string);
+	  $data = str_replace(array('+','/','='),array('-','_',':'),$data);
+	  return $data;
+	}
+
+	static function decryptURL($string)
+	{
+	  $data = str_replace(array('-','_',':'),array('+','/','='),$string);
+	  $mod4 = strlen($data) % 4;
+	  if ($mod4) {
+	    $data .= substr('====', $mod4);
+	  } 
+	  return base64_decode($data);
+	}
+
+	static function isValidURL($url)
+	{
+		$regex = "^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$^"; 
+		if(preg_match($regex, $url))
 		{
-			$url_str = substr($url,strpos($url, 'www'));
-			if( in_array(substr_count($url_str, '.'), array(2,3)) )
-			return TRUE;
+			if(strstr($url,'www'))
+			{
+				$url_str = substr($url,strpos($url, 'www'));
+				if( in_array(substr_count($url_str, '.'), array(2,3)) )
+				return TRUE;
+				else
+				return FALSE;
+			}
 			else
-			return FALSE;
+				return TRUE;
 		}
 		else
-			return TRUE;
+			return FALSE;
 	}
-	else
-		return FALSE;
-}
-/**
- * Function to escape value.
- **/
+	/**
+	 * Function to escape value.
+	 **/
 
-function escape($var)
-{
-	if( empty($var) )
-		return FALSE;
-
-	$var = trim($var);
-	$var = strip_tags($var);
-	$var = addslashes($var);
-	$var = htmlentities($var, ENT_QUOTES);
-	return $var;
-}
-
-/**
- * Function to get file extension
- */
-function getExtension($filename) {
-	if($filename){
-		$info = pathinfo($filename);
-		return strtolower($info['extension']);
-	}
-	return FALSE;
-}
-
-/**
-* Function to get file type by given extension.
-**/
- function getFileType( $ext ) {
-		$ext2type = array(
-			'audio'       => array( 'aac', 'ac3',  'aif',  'aiff', 'm3a',  'm4a',   'm4b', 'mka', 'mp1', 'mp2',  'mp3', 'ogg', 'oga', 'ram', 'wav', 'wma' ),
-			'video'       => array( 'asf', 'avi',  'divx', 'dv',   'flv',  'm4v',   'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'mpv', 'ogm', 'ogv', 'qt',  'rm', 'vob', 'wmv' ),
-			'document'    => array( 'doc', 'docx', 'docm', 'dotm', 'odt',  'pages', 'pdf', 'rtf', 'wp',  'wpd' ),
-			'spreadsheet' => array( 'numbers',     'ods',  'xls',  'xlsx', 'xlsb',  'xlsm' ),
-			'interactive' => array( 'key', 'ppt',  'pptx', 'pptm', 'odp',  'swf' ),
-			'text'        => array( 'asc', 'csv',  'tsv',  'txt' ),
-			'archive'     => array( 'bz2', 'cab',  'dmg',  'gz',   'rar',  'sea',   'sit', 'sqx', 'tar', 'tgz',  'zip' ),
-			'code'        => array( 'css', 'htm',  'html', 'php',  'js' ),
-		);
-		foreach ( $ext2type as $type => $exts )
-			if ( in_array( $ext, $exts ) )
-				return $type;
-	}
-
-/**
- * Function to generates a random password drawn from the defined set of characters.
- **/
-function generateKey( $length = 12, $special_chars = true, $extra_special_chars = false ) {
-	$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-	if ( $special_chars )
-		$chars .= '!@#$%^&*()';
-	if ( $extra_special_chars )
-		$chars .= '-_ []{}<>~`+=,.;:/?|';
-
-	$password = '';
-	for ( $i = 0; $i < $length; $i++ ) {
-		$password .= substr($chars, self::_rand(0, strlen($chars) - 1), 1);
-	}
-	return $password;
-}
-
-/*
- * Function to generate a random number.
- */
-function _rand( $min = 0, $max = 0 ) {
-	$rnd_value='';
-
-	if ( strlen($rnd_value) < 8 ) {
-		$rnd_value = md5( uniqid(microtime() . mt_rand(), true ) );
-		$rnd_value .= sha1($rnd_value);
-		$seed = md5($rnd_value);
-	}
-	// Take the first 8 digits for our value
-	$value = substr($rnd_value, 0, 8);
-	$rnd_value = substr($rnd_value, 8);
-	$value = abs(hexdec($value));
-
-	if ( $max != 0 )
-		$value = $min + (($max - $min + 1) * ($value / (4294967295 + 1)));
-
-	return abs(intval($value));
-}
-
-/**
- * Function to get values from ($_POST or $_GET methods) otherwise set to empty.
- */
-function getVars($vars=array()){
-	for($i = 0; $i < count($vars); $i++){
-		$var = $vars[$i];
-		global $$var;
-		if( !isset($$var) ){
-			if(empty($_REQUEST[$var]))
-				$$var = "";
-			else
-				$$var = self::escape($_REQUEST[$var]);
-		}
-	}
-}
-
-function _print_r($var)
-{
-	if( empty($var) )
-		return false;
-		
-	print "<fieldset style=\"border: 1px solid black; font:12px 'Courier new',monospace; padding:10px; margin:10px 0;\">";
-	print "<legend>";
-	print "<b>PRINT RESULT</b>";
-	print "</legend>";
-	print "<pre>";
-	print_r($var);
-	print "</pre>";
-	print "</fieldset>";
-}
-
-function showQuery($query, $params)
-{
-	$keys = array();
-	$values = array();
-  
-	# build a regular expression for each parameter
-
-	foreach ($params as $key=>$value)
+	static function escape($var)
 	{
-		if (is_string($key)){
-			$keys[] = '/:'.$key.'/';
+		if( empty($var) )
+			return FALSE;
+
+		$var = trim($var);
+		$var = strip_tags($var);
+		$var = addslashes($var);
+		$var = htmlentities($var, ENT_QUOTES);
+		return $var;
+	}
+
+	/**
+	 * Function to get file extension
+	 */
+	static function getExtension($filename) {
+		if($filename){
+			$info = pathinfo($filename);
+			return strtolower($info['extension']);
 		}
-		else{
-			$keys[] = '/[?]/';
-		}           
-		if(is_numeric($value)){
-			$values[] = intval($value);
+		return FALSE;
+	}
+
+	/**
+	* Function to get file type by given extension.
+	**/
+	 static function getFileType( $ext ) {
+			$ext2type = array(
+				'audio'       => array( 'aac', 'ac3',  'aif',  'aiff', 'm3a',  'm4a',   'm4b', 'mka', 'mp1', 'mp2',  'mp3', 'ogg', 'oga', 'ram', 'wav', 'wma' ),
+				'video'       => array( 'asf', 'avi',  'divx', 'dv',   'flv',  'm4v',   'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'mpv', 'ogm', 'ogv', 'qt',  'rm', 'vob', 'wmv' ),
+				'document'    => array( 'doc', 'docx', 'docm', 'dotm', 'odt',  'pages', 'pdf', 'rtf', 'wp',  'wpd' ),
+				'spreadsheet' => array( 'numbers',     'ods',  'xls',  'xlsx', 'xlsb',  'xlsm' ),
+				'interactive' => array( 'key', 'ppt',  'pptx', 'pptm', 'odp',  'swf' ),
+				'text'        => array( 'asc', 'csv',  'tsv',  'txt' ),
+				'archive'     => array( 'bz2', 'cab',  'dmg',  'gz',   'rar',  'sea',   'sit', 'sqx', 'tar', 'tgz',  'zip' ),
+				'code'        => array( 'css', 'htm',  'html', 'php',  'js' ),
+			);
+			foreach ( $ext2type as $type => $exts )
+				if ( in_array( $ext, $exts ) )
+					return $type;
 		}
-		else{
-			$values[] = '"'.$value .'"';
+
+	/**
+	 * Function to generates a random password drawn from the defined set of characters.
+	 **/
+	static function generateKey( $length = 12, $special_chars = true, $extra_special_chars = false ) {
+		$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		if ( $special_chars )
+			$chars .= '!@#$%^&*()';
+		if ( $extra_special_chars )
+			$chars .= '-_ []{}<>~`+=,.;:/?|';
+
+		$password = '';
+		for ( $i = 0; $i < $length; $i++ ) {
+			$password .= substr($chars, self::_rand(0, strlen($chars) - 1), 1);
+		}
+		return $password;
+	}
+
+	/*
+	 * Function to generate a random number.
+	 */
+	static function _rand( $min = 0, $max = 0 ) {
+		$rnd_value='';
+
+		if ( strlen($rnd_value) < 8 ) {
+			$rnd_value = md5( uniqid(microtime() . mt_rand(), true ) );
+			$rnd_value .= sha1($rnd_value);
+			$seed = md5($rnd_value);
+		}
+		// Take the first 8 digits for our value
+		$value = substr($rnd_value, 0, 8);
+		$rnd_value = substr($rnd_value, 8);
+		$value = abs(hexdec($value));
+
+		if ( $max != 0 )
+			$value = $min + (($max - $min + 1) * ($value / (4294967295 + 1)));
+
+		return abs(intval($value));
+	}
+
+	/**
+	 * Function to get values from ($_POST or $_GET methods) otherwise set to empty.
+	 */
+	static function getVars($vars=array()){
+		for($i = 0; $i < count($vars); $i++){
+			$var = $vars[$i];
+			global $$var;
+			if( !isset($$var) ){
+				if(empty($_REQUEST[$var]))
+					$$var = "";
+				else
+					$$var = self::escape($_REQUEST[$var]);
+			}
 		}
 	}
-	$query = preg_replace($keys, $values, $query, 1, $count);
-	//echo "DSGFDGFHGJFDGSDDFG";
-	print $query;        
+
+	static function showQuery($query, $params)
+	{
+		$keys = array();
+		$values = array();
+	  
+		# build a regular expression for each parameter
+
+		foreach ($params as $key=>$value)
+		{
+			if (is_string($key)){
+				$keys[] = '/:'.$key.'/';
+			}
+			else{
+				$keys[] = '/[?]/';
+			}           
+			if(is_numeric($value)){
+				$values[] = intval($value);
+			}
+			else{
+				$values[] = '"'.$value .'"';
+			}
+		}
+		$query = preg_replace($keys, $values, $query, 1, $count);
+		//echo "DSGFDGFHGJFDGSDDFG";
+		print $query;        
+	}
 }
